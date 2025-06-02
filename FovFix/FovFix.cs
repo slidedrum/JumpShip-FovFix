b771b718af4517b7d1fb7b106f57c62f7ee3fae0
slidedrum <donny895@aol.com>
2025-06-02 18:13:44 -0400

    Add project files.

---
+++ FovFix/FovFix.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using MelonLoader;
+
+namespace FovFix
+{
+    public class FovFixMain :MelonMod
+    {
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (Camera.main != null && Camera.main.fieldOfView == 65)
+            {
+                Camera.main.fieldOfView = 105;
+            }
+        }
+    }
+}
---
+++ FovFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using FovFix; // The namespace of your mod class
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+// ...
+[assembly: MelonInfo(typeof(FovFixMain), "Fov Fix", "0.0.1", "SlideDrum")]
+[assembly: MelonGame("KeepsakeGames", "Jump Ship")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FovFix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FovFix")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35793077-c3ea-4662-8dec-d5c19e7cb264")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
